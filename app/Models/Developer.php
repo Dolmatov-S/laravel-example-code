0cc005eea62818cc7611bc3384af4243549b1f67
Dolmatov <dolmatov.me@gmail.com>
2024-04-03 14:21:29 +0300

    refactor: Developer site order migration and set relation
    Изменил очередь миграции результирующей таблицы. Теперь она выполняется после таблицы developer_frameworks. Это изменение вносит ясность в отношение разработчиков к сайтам. После этого коммита, разработчики сайта должны владеть фреймворком, на котором написан сайт. Добавил отношение застройщика к фреймворку
    
     - 2024_04_03_102641_create_developer_sites_table.php => 2024_04_03_105641_create_developer_sites_table.php - Переименовал время миграции, чтобы отпустить её под миграцию developer_frameworks

--- app/Models/Developer.php
+++ app/Models/Developer.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Developer extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'email', 'salary'];
 
+    public function framework(): HasMany
+    {
+        return $this->hasMany(DeveloperFramework::class);
+    }
 }
