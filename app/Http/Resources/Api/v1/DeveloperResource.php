77597aa1cb6f08b49a59ec1686d79e4896af6577
Dolmatov <dolmatov.me@gmail.com>
2024-04-05 16:32:56 +0300

    feat: developer resource
    Добавил ресурс информации о разработчике. Он формирует единый формат данных о разработчике
    
    DeveloperResource.php

---
+++ app/Http/Resources/Api/v1/DeveloperResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\Api\v1;
+
+use App\Models\Framework;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DeveloperResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'site_count' => $this->site()->count(),
+            'site_link' => route('sites', ['developer_id' => $this->id]),
+
+            'frameworks' => Framework::whereIn('id', $this->framework()->select('framework_id')->get()->toArray())
+                ->select('name')
+                ->get()
+                ->implode('name', ', '),
+            'salary' => $this->salary,
+        ];
+    }
+}
