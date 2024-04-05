9ea5c3455767d7d69a87f4df85a2c1b4db855f88
Dolmatov <dolmatov.me@gmail.com>
2024-04-05 11:41:30 +0300

    feat: Filter Developer multiple select
    Добавил первых элемент управления фильтра. Селектор с множественным выбором разработчика.
    
    - filter-developer-select.blade.php - Шаблон селектора
    - FilterDeveloperSelectComposer.php - Сборщик селектора. Извлекает из репозитория список разработчиков с выбранными пользователями элементами и передаёт их в шаблон селектора

---
+++ app/View/Composers/Filter/FilterDeveloperSelectComposer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Composers\Filter;
+
+use App\Http\Requests\Filter\QueryFilterRequest;
+use App\Repositories\FilterRepository;
+use Hoa\Iterator\Filter;
+use Illuminate\View\View;
+
+
+class FilterDeveloperSelectComposer {
+
+    private QueryFilterRequest $queryFilterRequest;
+    private  FilterRepository $filterRepository;
+
+    public function __construct(QueryFilterRequest $queryFilterRequest, FilterRepository $filterRepository)
+    {
+        $this->queryFilterRequest = $queryFilterRequest;
+        $this->filterRepository = $filterRepository;
+    }
+
+    public function compose(View $view)
+    {
+        $developers = $this->filterRepository->getDevelopersWitchRequestSelect($this->queryFilterRequest->get('developer_id'));
+        $view->with('developers', $developers);
+    }
+
+}
